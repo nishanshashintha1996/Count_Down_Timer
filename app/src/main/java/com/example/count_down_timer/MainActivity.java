855edf979c515b17116f4ab0c6625391ded19497
nishanshashintha1996 <shashinthanishan00@gmail.com>
2019-03-06 16:55:41 +0530

    06 March 2019 Edit

--- app/src/main/java/com/example/count_down_timer/MainActivity.java
+++ app/src/main/java/com/example/count_down_timer/MainActivity.java
@@ -1,13 +1,33 @@
 package com.example.count_down_timer;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+private int counter = 99;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    protected void countdown(View view){
+        final TextView textView = findViewById(R.id.textView);
+
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                textView.setText(Integer.toString(counter));
+                counter--;
+                handler.postDelayed(this,100);
+                if(counter==0){
+                    counter=99;
+                }
+            }
+        });
     }
 }
